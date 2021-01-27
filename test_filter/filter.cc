56df77c9802de4c33267ca250fbe47d331ab80b0
Carlos Alberto Salazar Herrera <casalazarh@unal.edu.co>
2021-01-27 01:14:19 -0500

    Adding a new folder to integrate filter testing

---
+++ test_filter/filter.cc
@@ -0,0 +1,37 @@
+
+
+
+/*
+* Copyright(c) 2019 Netflix, Inc.
+*
+* This source code is subject to the terms of the BSD 2 Clause License and
+* the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+* was not distributed with this source code in the LICENSE file, you can
+* obtain it at https://www.aomedia.org/license/software-license. If the Alliance for Open
+* Media Patent License 1.0 was not distributed with this source code in the
+* PATENTS file, you can obtain it at https://www.aomedia.org/license/patent-license.
+*/
+
+/******************************************************************************
+ * @file wiener_convolve_test.cc
+ *
+ * @brief Unit test of wiener convolve add source:
+ * - av1_wiener_convolve_add_src_avx2
+ * - av1_highbd_wiener_convolve_add_src_avx2
+ *
+ * @author Cidana-Edmond
+ *
+ ******************************************************************************/
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    cout << "Hello, World!:" << endl;
+    return 0;
+
+
+}
